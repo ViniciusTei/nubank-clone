1b6d8643ddffa7e1623d4b25d76786cfe0f39329
ViniciusTei <viniciustprates@gmail.com>
2020-03-31 09:06:47 -0300

    Styled components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import { Container } from './styles';
 
 export default function Home() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-        </View>
+        <Container/>
     );
 }
